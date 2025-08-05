460c0dcbdfc6fe292d3d3b766faa8d260423af95
duncai233 <155240659+duncai233@users.noreply.github.com>
2025-08-06 04:54:38 +0800

    修改包名

--- app/src/main/java/icu/nullptr/hidemyapplist/ui/fragment/LogsFragment.kt
+++ app/src/main/java/icu/nullptr/hidemyapplist/ui/fragment/LogsFragment.kt
@@ -9,8 +9,8 @@ import androidx.lifecycle.lifecycleScope
 import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.LinearLayoutManager
 import by.kirich1409.viewbindingdelegate.viewBinding
-import com.tsng.hidemyapplist.R
-import com.tsng.hidemyapplist.databinding.FragmentLogsBinding
+import com.tencent.letsgo.R
+import com.tencent.letsgo.databinding.FragmentLogsBinding
 import icu.nullptr.hidemyapplist.hmaApp
 import icu.nullptr.hidemyapplist.service.PrefManager
 import icu.nullptr.hidemyapplist.service.ServiceClient
